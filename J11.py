7df604af4ae7ca301c5e6cc8361417a454ddb7f0
Sangwoo Ji <sanji@meta.com>
2024-04-23 22:13:05 -0700

    Initial commit

---
+++ J11.py
@@ -0,0 +1,18 @@
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+img = plt.imread('C://aiwork/full_forg/forgeries_1_1.png')
+plt.imshow(img)
+
+
+
+
+
+
+
+
+
